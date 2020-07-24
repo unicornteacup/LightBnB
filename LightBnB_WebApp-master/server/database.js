ec9943388b7e4803131d827bcae8caafc2042684
Caroline <carolinejepps@gmail.com>
2020-07-24 03:48:53 +0000

    Updated add property query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -59,7 +59,8 @@ const addUser =  function(user) {
   INSERT INTO users (name, email, password)
   VALUES ($1, $2, $3)
   RETURNING *`, [user.name, user.email, user.password])
-  .then(res => res.rows[0]);
+  .then(res => res.rows[0])
+  .catch('Error adding user');
 }
 exports.addUser = addUser;
 
@@ -82,7 +83,9 @@ const getAllReservations = function(guest_id, limit = 10) {
   GROUP BY properties.id, reservations.id
   ORDER BY reservations.start_date
   LIMIT $2`, [guest_id, limit])
-  .then(res => res.rows);
+  .then(res => res.rows)
+  .catch('Error accessing past reservations');
+
 }
 exports.getAllReservations = getAllReservations;
 
@@ -165,7 +168,8 @@ const getAllProperties = function(options, limit = 10) {
   console.log(queryString, queryParams);
 
   return pool.query(queryString, queryParams)
-  .then(res => res.rows);
+  .then(res => res.rows)
+  .catch('Unable to find matching properties');
   
   }
 exports.getAllProperties = getAllProperties;
@@ -177,9 +181,12 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+
+  return pool.query(`
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *`, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night * 100, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+  .then(res => res.rows[0])
+  .catch('Unable to add property');
 }
 exports.addProperty = addProperty;
